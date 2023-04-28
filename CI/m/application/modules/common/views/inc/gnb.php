7d8845dae9dac8aa70886a4814e03b63cbd84c47
kimyounjung <snail@rocateer.com>
2023-04-28 09:22:22 +0900

    gnb 수정

--- CI/m/application/modules/common/views/inc/gnb.php
+++ CI/m/application/modules/common/views/inc/gnb.php
@@ -2,12 +2,16 @@
 	<div class="main_header">
 		<a href="#"><img src="/images/head_btn_search.png" class="main_search"></a>
 		<a href="#" class="btn_gray_ghost btn_sm">로그인</a>
+		<a href="#" class="btn_gray_ghost btn_sm">MY</a>
 		<a href="#" class="btn_point btn_sm">글쓰기</a>
 	</div>
 </header>
 
 <ul class="gnb_wrap">
 	<li class="active"><a href="#">홈</a></li>
+	<li><a href="#">게시판1</a></li>
+	<li><a href="#">게시판2</a></li>
+	<li><a href="#">게시판3</a></li>
 	<li><a href="#">중고거래</a></li>
 	<li><a href="#">구인구직</a></li>
 	<li><a href="#">공동구매</a></li>
